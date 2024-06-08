485d5e9e12d5825d831e5b03aa4d8c5cf1080391
Orel <ryiseld@gmail.com>
2024-06-08 10:03:40 +0300

    add jinja test

---
+++ internal/engine/jinja_test.go
@@ -0,0 +1,55 @@
+package engine
+
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestJinjaRender(t *testing.T) {
+	dir := os.TempDir()
+	file, err := os.CreateTemp(dir, "test.txt")
+	require.NoError(t, err)
+
+	_, err = file.WriteString("Hello, {{ Name }}! You are {{ Age }} years old.")
+	require.NoError(t, err)
+
+	engine := &JinjaEngine{}
+	writer := &bytes.Buffer{}
+	err = engine.Render(file.Name(), writer, map[string]any{
+		"Name": "John",
+		"Age":  20,
+	})
+	require.NoError(t, err)
+	require.Equal(t, "Hello, John! You are 20 years old.", writer.String())
+}
+
+func TestJinjaRenderAdvanced(t *testing.T) {
+	dir := os.TempDir()
+	file, err := os.CreateTemp(dir, "test.txt")
+	require.NoError(t, err)
+
+	_, err = file.WriteString(`
+{% macro foo(v) -%}
+	Version is: {{ v }}
+{%- endmacro %}
+
+{{ foo(version) }}
+`)
+	require.NoError(t, err)
+
+	engine := &JinjaEngine{}
+	writer := &bytes.Buffer{}
+	err = engine.Render(file.Name(), writer, map[string]any{
+		"version": "1.2.3",
+	})
+
+	require.NoError(t, err)
+	require.Equal(t, `
+
+
+Version is: 1.2.3
+`, writer.String())
+}
