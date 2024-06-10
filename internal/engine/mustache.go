8feda1254cae4ea3917f29d752c1e51b2fad28c1
Orel <ryiseld@gmail.com>
2024-06-10 15:35:29 +0300

    rename out variable in mustache

--- internal/engine/mustache.go
+++ internal/engine/mustache.go
@@ -10,12 +10,12 @@ import (
 type MustacheEngine struct{}
 
 func (e *MustacheEngine) Render(file string, w io.Writer, data map[string]any) error {
-	out, err := mustache.RenderFile(file, data)
+	result, err := mustache.RenderFile(file, data)
 	if err != nil {
 		return fmt.Errorf("render %q: %s", file, err)
 	}
 
-	_, err = io.WriteString(w, out)
+	_, err = io.WriteString(w, result)
 	if err != nil {
 		return fmt.Errorf("write %q: %s", file, err)
 	}
