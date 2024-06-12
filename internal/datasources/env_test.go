24c816ba0ffac3fe5ce0b54ad86d1d71d33665a7
Aviad Haham <aviadhaham@outlook.com>
2024-06-12 15:30:34 +0300

    align tests with envparse

--- internal/datasources/env_test.go
+++ internal/datasources/env_test.go
@@ -1,19 +1,26 @@
 package datasources
 
 import (
+	"bytes"
+	"io"
 	"os"
 	"strings"
 	"testing"
 
+	"github.com/hashicorp/go-envparse"
 	"github.com/stretchr/testify/require"
 )
 
 func TestEnvLoad(t *testing.T) {
-	expectedData := map[string]any{}
-	envVars := os.Environ()
-	for _, envVar := range envVars {
-		envVar := strings.Split(envVar, "=")
-		expectedData[envVar[0]] = envVar[1]
+	var expectedData = map[string]any{}
+	var r io.Reader
+
+	// Test environment variables from OS
+	r = bytes.NewReader([]byte(strings.Join(os.Environ(), "\n")))
+	env, err := envparse.Parse(r)
+	require.NoError(t, err)
+	for k, v := range env {
+		expectedData[k] = v
 	}
 
 	ds := NewEnvDatasource("")
@@ -21,17 +28,24 @@ func TestEnvLoad(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, expectedData, data)
 
+	// Test environment variables from file
+	clear(expectedData)
+	envVars := []string{"key1=value1", "key2=5"}
+	r = bytes.NewReader([]byte(strings.Join(envVars, "\n")))
+	env, err = envparse.Parse(r)
+	require.NoError(t, err)
+	for k, v := range env {
+		expectedData[k] = v
+	}
+
 	dir := t.TempDir()
 	file, err := os.CreateTemp(dir, ".env")
 	require.NoError(t, err)
-	_, err = file.WriteString(`key1=value1
+	_, err = file.WriteString(`
+key1=value1
 key2=5`)
 	require.NoError(t, err)
 
-	expectedData = map[string]any{
-		"key1": "value1",
-		"key2": "5",
-	}
 	ds = NewEnvDatasource(file.Name())
 	data, err = ds.Load()
 	require.NoError(t, err)
