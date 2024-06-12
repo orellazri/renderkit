6d8bc9d056ab5a3a24477ca60c57718e261f84f2
Aviad Haham <aviadhaham@outlook.com>
2024-06-12 15:04:56 +0300

    env vars with envparse - improvements

--- internal/datasources/env.go
+++ internal/datasources/env.go
@@ -3,6 +3,7 @@ package datasources
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
@@ -19,29 +20,26 @@ func NewEnvDatasource(filepath string) *EnvDatasource {
 
 func (ds *EnvDatasource) Load() (map[string]any, error) {
 	data := make(map[string]any)
-	if ds.filepath == "" {
-		r := bytes.NewReader([]byte(strings.Join(os.Environ(), "\n")))
-		env, err := envparse.Parse(r)
-		if err != nil {
-			return nil, fmt.Errorf("parse environment variables: %s", err)
-		}
-		for k, v := range env {
-			data[k] = v
-		}
+	var r io.Reader
+
+	if ds.filepath == "" { // If no file is provided, we use the current environment variables
+		r = bytes.NewReader([]byte(strings.Join(os.Environ(), "\n")))
 	} else {
 		f, err := os.Open(ds.filepath)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("open file %s: %s", ds.filepath, err)
 		}
 		defer f.Close()
-
-		env, err := envparse.Parse(f)
-		if err != nil {
-			return nil, fmt.Errorf("parse environment variables from file %s: %s", ds.filepath, err)
-		}
-		for k, v := range env {
-			data[k] = v
-		}
+		r = f
 	}
+
+	env, err := envparse.Parse(r)
+	if err != nil {
+		return nil, fmt.Errorf("parse environment variables: %s", err)
+	}
+	for k, v := range env {
+		data[k] = v
+	}
+
 	return data, nil
 }
