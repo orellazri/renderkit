cfa4bfee999a29bcdf767c1181da5a0cda9f17e9
Orel <ryiseld@gmail.com>
2024-06-15 13:40:44 +0300

    refactor(render): simplify input and output choices

--- internal/app/render.go
+++ internal/app/render.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"bytes"
+	"context"
 	"errors"
 	"fmt"
 	"io"
@@ -18,38 +19,27 @@ func (a *App) render(
 	excluded []string,
 	data map[string]any,
 ) error {
-	var output io.Writer
-	if len(outputDir) > 0 {
-		if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
-			return fmt.Errorf("create output directory %s: %s", outputDir, err)
-		}
-	} else {
-		output = os.Stdout
-	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var output io.Writer = os.Stdout
+	var err error
 
 	if len(inputString) > 0 { // Render input string
 		if len(outputDir) > 0 {
-			outputFilename := filepath.Join(outputDir, "renderkit_output")
-			outputFile, err := os.Create(outputFilename)
+			output, err = createOutputFileWithDir(ctx, filepath.Join(outputDir, "renderkit_output"))
 			if err != nil {
-				return fmt.Errorf("create output file %s: %s", outputFilename, err)
+				return err
 			}
-			defer outputFile.Close()
-			output = outputFile
 		}
-
 		return a.renderString(inputString, output, data)
 	} else if len(inputFile) > 0 { // Render input file
 		if len(outputDir) > 0 {
-			outputFilename := filepath.Join(outputDir, filepath.Base(inputFile))
-			outputFile, err := os.Create(outputFilename)
+			output, err = createOutputFileWithDir(ctx, filepath.Join(outputDir, filepath.Base(inputFile)))
 			if err != nil {
-				return fmt.Errorf("create output file %s: %s", outputFilename, err)
+				return err
 			}
-			defer outputFile.Close()
-			output = outputFile
 		}
-
 		return a.renderFile(inputFile, output, excluded, data)
 	} else if len(inputDir) > 0 { // Render input directory
 		return a.renderDir(inputDir, outputDir, excluded, data)
@@ -58,8 +48,8 @@ func (a *App) render(
 	return errors.New("unsupported mode")
 }
 
-func (a *App) renderDir(inputDirname string, outputDirname string, excluded []string, data map[string]any) error {
-	err := filepath.WalkDir(inputDirname, func(path string, d os.DirEntry, err error) error {
+func (a *App) renderDir(inputDirpath string, outputDirpath string, excluded []string, data map[string]any) error {
+	err := filepath.WalkDir(inputDirpath, func(path string, d os.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
@@ -68,27 +58,20 @@ func (a *App) renderDir(inputDirname string, outputDirname string, excluded []st
 			return nil
 		}
 
-		relPath, err := filepath.Rel(inputDirname, path)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		relPath, err := filepath.Rel(inputDirpath, path)
 		if err != nil {
 			return fmt.Errorf("get relative path: %s", err)
 		}
 
-		var output io.Writer
-		if len(outputDirname) > 0 {
-			// Create output subdirectory
-			err = os.MkdirAll(filepath.Join(outputDirname, filepath.Dir(relPath)), os.ModePerm)
+		var output io.Writer = os.Stdout
+		if len(outputDirpath) > 0 {
+			output, err = createOutputFileWithDir(ctx, filepath.Join(outputDirpath, relPath))
 			if err != nil {
-				return fmt.Errorf("create output directory %q: %s", outputDirname, err)
+				return err
 			}
-
-			outputFilename := filepath.Join(outputDirname, relPath)
-			outputFile, err := os.Create(outputFilename)
-			if err != nil {
-				return fmt.Errorf("create output file %q: %s", outputFilename, err)
-			}
-			output = outputFile
-		} else {
-			output = os.Stdout
 		}
 		if err := a.renderFile(path, output, excluded, data); err != nil {
 			return fmt.Errorf("render file %q: %s", path, err)
@@ -97,18 +80,18 @@ func (a *App) renderDir(inputDirname string, outputDirname string, excluded []st
 		return nil
 	})
 	if err != nil {
-		return fmt.Errorf("walk directory %q: %s", inputDirname, err)
+		return fmt.Errorf("walk directory %q: %s", inputDirpath, err)
 	}
 
 	return nil
 }
 
-func (a *App) renderFile(inputFilename string, output io.Writer, excluded []string, data map[string]any) error {
-	if slices.Contains(excluded, inputFilename) {
+func (a *App) renderFile(inputFilepath string, output io.Writer, excluded []string, data map[string]any) error {
+	if slices.Contains(excluded, inputFilepath) {
 		return nil
 	}
 
-	if err := a.engine.RenderFile(inputFilename, output, data); err != nil {
+	if err := a.engine.RenderFile(inputFilepath, output, data); err != nil {
 		return fmt.Errorf("render template: %s", err)
 	}
 
@@ -121,4 +104,22 @@ func (a *App) renderString(inputString string, output io.Writer, data map[string
 	}
 
 	return nil
+}
+
+func createOutputFileWithDir(ctx context.Context, outputFilepath string) (io.Writer, error) {
+	outputDirpath := filepath.Dir(outputFilepath)
+	if err := os.MkdirAll(outputDirpath, os.ModePerm); err != nil {
+		return nil, fmt.Errorf("create output directory %s: %s", outputDirpath, err)
+	}
+
+	outputFile, err := os.Create(outputFilepath)
+	if err != nil {
+		return nil, fmt.Errorf("create output file %s: %s", outputFilepath, err)
+	}
+	go func(ctx context.Context) {
+		<-ctx.Done()
+		outputFile.Close()
+	}(ctx)
+
+	return outputFile, nil
 }
