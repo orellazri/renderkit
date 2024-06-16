649703d7b0e1fc5426aabd0b33df08c37804221f
Aviad Haham <aviadhaham@outlook.com>
2024-06-16 10:12:37 +0300

    fix(render): remove redundant exclude check from renderFile

--- internal/app/render.go
+++ internal/app/render.go
@@ -39,7 +39,7 @@ func (a *App) render(
 			}
 			defer closer()
 		}
-		return a.renderFile(inputFile, output, excluded, data)
+		return a.renderFile(inputFile, output, data)
 	} else if len(inputDir) > 0 { // Render input directory
 		return a.renderDir(inputDir, outputDir, excluded, data)
 	}
@@ -74,7 +74,7 @@ func (a *App) renderDir(inputDirpath string, outputDirpath string, excluded []st
 			}
 			defer closer()
 		}
-		if err := a.renderFile(path, output, excluded, data); err != nil {
+		if err := a.renderFile(path, output, data); err != nil {
 			return fmt.Errorf("render file %q: %s", path, err)
 		}
 
@@ -87,11 +87,7 @@ func (a *App) renderDir(inputDirpath string, outputDirpath string, excluded []st
 	return nil
 }
 
-func (a *App) renderFile(inputFilepath string, output io.Writer, excluded []string, data map[string]any) error {
-	if slices.Contains(excluded, inputFilepath) {
-		return nil
-	}
-
+func (a *App) renderFile(inputFilepath string, output io.Writer, data map[string]any) error {
 	if err := a.engine.RenderFile(inputFilepath, output, data); err != nil {
 		return fmt.Errorf("render template: %s", err)
 	}
