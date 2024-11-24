92b1c3cd318fb786e497b39a2647615f948daf80
Orel <ryiseld@gmail.com>
2024-11-24 11:37:03 +0200

    fix: use debug to read version through build info

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"runtime/debug"
 
 	"github.com/orellazri/renderkit/internal/app"
 )
@@ -11,6 +12,12 @@ import (
 var version = "dev"
 
 func main() {
+	// If the version is not set at compile time, try to get it from the build info
+	info, ok := debug.ReadBuildInfo()
+	if ok && info.Main.Version != "(devel)" {
+		version = info.Main.Version
+	}
+
 	app := app.NewApp(version)
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
